a46b2713b95f72d1dd9bcef2856fcd9c090464a3
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-12-01 15:22:48 +0300

    custom methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,15 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  
+  has_many :comments, class_name: 'Comment'
+  has_many :likes, class_name: 'Like'
+
+  after_save :update_post_counter
+
+  def update_post_counter
+    author.increment!(:post_counter)
+  end
+
+  def five_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
-  has_mnay :posts, class_name: 'Post'
+  has_many :posts, class_name: 'Post'
   has_many :comments, class_name: 'Comment'
   has_many :likes, class_name: 'Like'
+
+
+  def three_recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end 
 end

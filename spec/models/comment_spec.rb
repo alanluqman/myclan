8ec585ac55323006f2020248604eb09ad4811ae2
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-12-01 21:34:24 +0300

    Create comment_spec.rb

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+
+  subject do
+    user = User.create(name: 'alan', photo: 'http://picture', bio: 'I am developer')
+    post = Post.create(title: 'new post', text: 'this is first post', author: user)
+    Comment.create(post: post, author: user, text: 'Hi Alan!')
+  end
+
+  it '# is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'comment_counter get incremented by 1' do
+    expect(subject.post.comment_counter).to eq(1)
+  end
+
+end

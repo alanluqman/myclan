ce83fb62adf4fe7c92df6269370f57937dc9abab
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-12-01 16:13:18 +0300

    fix association foreign key

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :comments, class_name: 'Comment'
-  has_many :likes, class_name: 'Like'
+  has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
+  has_many :likes, class_name: 'Like', foreign_key: 'post_id'
 
   after_save :update_post_counter
 

c660c9d2133107f3689bac739cb4488174d172d0
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-12-10 11:46:11 +0300

    user ca comment any post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,35 +1,22 @@
 class CommentsController < ApplicationController
-
   def new
     @comment = Comment.new
   end
 
-#   def create
-#     @user = current_user
-#     @post = Post.find(params[:post_id])
-#     new_comment = Comment.new(author_id: current_user.id, post_id: @post.id, text: comment_text)
-#     if new_comment.save
-#       redirect_to user_post_path(user_id: current_user.id, id: @post.id)
-#     else
-#       flash[:notice] = 'comment does not saved !'
-#     end
-#   end
-
-def create
-   @post = Post.find(params[:post_id])
-   @user = current_user
-    new_comment = current_user.comments.new(post_id: @post.id,
-                                            author_id: current_user.id, text: comment_text)
+  def create
+    @post = Post.find(params[:post_id])
+    @user = current_user
+    new_comment = current_user.comments.new(comment_params)
     if new_comment.save
-      redirect_to post_comments_path, notice: 'Success Comment Saved!'
+      redirect_to user_post_path(user_id: @user.id, id: @post.id), notice: 'Success Comment Saved!'
     else
-      render :new, status: 'Error occured with Comment!'
+      flash[:notice] = 'comment does not saved !'
     end
-
   end
 
+  private
 
-  def comment_text
-    params.require(:comments).permit(:text)
+  def comment_params
+    params.permit(:text, :author_id, :post_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   # root path is '/' or '/users'
   root 'users#index'
-
   resources :users do
     resources :posts do
       resources :comments
     end
   end
-  
 end

2b3e6342f72ca15927636702df52a1129136b176
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-12-15 14:58:03 +0300

    Add devise to project

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # root path is '/' or '/users'
   root 'users#index'
+  devise_for :users
+  delete '/users/sign_out', to: 'devise/sessions#destroy', as: 'logout'
   resources :users do
     resources :posts do
       resources :comments
--- db/schema.rb
+++ db/schema.rb
@@ -1,6 +1,16 @@
-# rubocop:disable Metrics/BlockLength
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_221_129_162_425) do
+ActiveRecord::Schema[7.0].define(version: 20_221_214_090_559) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -41,6 +51,13 @@ ActiveRecord::Schema[7.0].define(version: 20_221_129_162_425) do
     t.integer 'post_counter', default: 0
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
   add_foreign_key 'comments', 'posts'
@@ -49,5 +66,3 @@ ActiveRecord::Schema[7.0].define(version: 20_221_129_162_425) do
   add_foreign_key 'likes', 'users', column: 'author_id'
   add_foreign_key 'posts', 'users', column: 'author_id'
 end
-
-# rubocop:enable Metrics/BlockLength

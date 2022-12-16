488dc3afc43c5312eec82d1a4901bd2e38d25980
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-12-16 19:14:44 +0300

    Create destroy action in comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,6 +14,18 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @user = @post.author
+
+    if @comment.destroy
+      @post.decrement!(:comment_counter)
+      redirect_to user_post_path(user_id: @user.id, id: @post.id)
+    end
+
+  end
+
   private
 
   def comment_params
